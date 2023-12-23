2aca6243374d6b251c07e37c09ea6c4012055620
damorest <pptreiding@gmail.com>
2023-12-23 02:39:46 +0200

    splash and login page

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,6 +1,8 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/const/colors.dart';
 import 'package:shop_app/const/strings.dart';
+import 'package:shop_app/screens/auth_screen/login_screen.dart';
 
 import '../../const/styles.dart';
 
@@ -14,37 +16,41 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return AnimatedSplashScreen(
+      splashIconSize: MediaQuery.of(context).size.height,
+      animationDuration: const Duration(seconds: 5),
       backgroundColor: redColor,
-      body: Center(
-        child: Align(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              const SizedBox(height: 30),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: whiteColor,
-                ),
-                padding: const EdgeInsets.all(8),
-                width: 77,
-                height: 77,
-                child: Image.asset(
-                    'assets/images/app_logo.png',
-                width: 300),
+      splash:Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: whiteColor,
               ),
-              const SizedBox(height: 10),
-              const Text(appname, style: TextStyle(fontFamily: bold, fontSize: 22, color: whiteColor),),
-              const SizedBox(height: 5),
-              const Text(appversion, style: TextStyle(color: whiteColor),),
-              const Spacer(),
-              const Text(credits, style: TextStyle(color: whiteColor, fontFamily: semibold),),
-              const SizedBox(height: 30),
-            ],
-          ),
+              padding: const EdgeInsets.all(8),
+              width: 77,
+              height: 77,
+              child: Image.asset(
+                  'assets/images/app_logo.png',
+                  width: 300),
+            ),
+            const SizedBox(height: 10),
+            const Text(appname, style: TextStyle(fontFamily: bold, fontSize: 22, color: whiteColor),),
+            const SizedBox(height: 12),
+            const Text(appversion, style: TextStyle(color: whiteColor),),
+            const Spacer(),
+            const Text(credits, style: TextStyle(color: whiteColor, fontFamily: semibold),),
+            const SizedBox(height: 30),
+          ],
         ),
       ),
-    );
+
+
+      nextScreen: const LoginPage(),);
+
   }
 }
---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/const/colors.dart';
+import 'package:shop_app/const/styles.dart';
+
+Widget customTextField({String? lable, String? hint, controller }) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(lable!, style: const TextStyle(color: redColor, fontFamily: semibold, fontSize: 16)),
+      TextFormField(
+        decoration: InputDecoration(
+          hintStyle: const TextStyle(
+            fontFamily: semibold,
+            color: fontGrey,
+          ),
+          hintText: hint,
+          isDense: true,
+          fillColor: lightGrey,
+          filled: true,
+          border: InputBorder.none,
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: redColor,
+            ),
+          ),
+        ),
+      ),
+      const SizedBox(height: 5),
+    ],
+  );
+}
